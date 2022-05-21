0f11066f2b99a9e89c25cb2708bbee8c15e3a6fe
Sergey Medvedev <s.medvedev86@mail.ru>
2022-05-21 23:44:46 +0300

    add Engine.java

---
+++ src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,61 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+
+    private int rangeBoundary;
+    private final int numberOfRounds = 3;
+    private int randomNumber;
+    private String rightAnswer;
+    private static String name = "";
+    private final static Scanner sc = new Scanner(System.in);
+
+    public Engine(int inputRangeBoundary) {
+        this.rangeBoundary = inputRangeBoundary;
+    }
+
+    public Engine() {
+    }
+
+    public final int getNumberOfRounds() {
+        return numberOfRounds;
+    }
+
+    public final int getRandomNumber() {
+        randomNumber = (int) ((Math.random() * rangeBoundary) + 1);
+        return randomNumber;
+    }
+
+    public final void setRightAnswer(String inputRightAnswer) {
+        this.rightAnswer = inputRightAnswer;
+    }
+
+    public final void greeting() {
+        System.out.println("\nWelcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public final void userInteraction() {
+        System.out.print("Your answer: ");
+        var userAnswer = sc.nextLine();
+        if (userAnswer.equals(rightAnswer)) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println(userAnswer + " is wrong answer ;(. Correct answer was " + rightAnswer + ".");
+            System.out.println("Let's try again, " + name + "!");
+            System.exit(0);
+        }
+    }
+    public static void getCongratulations() {
+        System.out.println("Congratulations, " + name + "!");
+    }
+}
+
+
+
+
+
+
