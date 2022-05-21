101f371152c4987d6e40673b871684abbc9a0771
Sergey Medvedev <s.medvedev86@mail.ru>
2022-05-21 23:47:27 +0300

    add FirstGame.java SecondGame.java

---
+++ src/main/java/hexlet/code/games/FirstGame.java
@@ -0,0 +1,22 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class FirstGame {
+    public static void even() {
+        Engine even = new Engine(100);
+        even.greeting();
+        System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+        for (int i = 0; i < even.getNumberOfRounds(); i++) {
+            int randomNumber = even.getRandomNumber();
+            if (randomNumber % 2 == 0) {
+                even.setRightAnswer("yes");
+            } else {
+                even.setRightAnswer("no");
+            }
+            System.out.println("Question: " + randomNumber);
+            even.userInteraction();
+        }
+        Engine.getCongratulations();
+    }
+}
---
+++ src/main/java/hexlet/code/games/SecondGame.java
@@ -0,0 +1,37 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class SecondGame {
+    private static String operation;
+    static final int COUNT_RANDOM_OPERATIONS = 3;
+    public static void calc() {
+        Engine calc = new Engine(20);
+        calc.greeting();
+        System.out.println("What is the result of the expression?");
+        for (int i = 0; i < calc.getNumberOfRounds(); i++) {
+            int randomNumber = calc.getRandomNumber();
+            int randomNumber1 = calc.getRandomNumber();
+
+            int randomOperation = (int) ((Math.random() * COUNT_RANDOM_OPERATIONS) + 1);
+            switch (randomOperation) {
+                case 1 -> {
+                    calc.setRightAnswer(Integer.toString(randomNumber + randomNumber1));
+                    operation = "+";
+                }
+                case 2 -> {
+                    calc.setRightAnswer(Integer.toString(randomNumber - randomNumber1));
+                    operation = "-";
+                }
+                case 3 -> {
+                    calc.setRightAnswer(Integer.toString(randomNumber * randomNumber1));
+                    operation = "*";
+                }
+                default -> System.exit(0);
+            }
+            System.out.println("Question: " + randomNumber + " " + operation + " " + randomNumber1);
+            calc.userInteraction();
+        }
+        Engine.getCongratulations();
+    }
+}
