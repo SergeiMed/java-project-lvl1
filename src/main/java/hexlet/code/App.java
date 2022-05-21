75fab73a23d7bf12d5f22dd7a96d088ca51a3811
Sergey Medvedev <s.medvedev86@mail.ru>
2022-05-21 23:47:50 +0300

    fix App.java Cli.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,16 +1,21 @@
 package hexlet.code;
 
+import hexlet.code.games.FirstGame;
+import hexlet.code.games.SecondGame;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
+        int userChoice;
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
         System.out.print("Your choice: ");
         Scanner scan = new Scanner(System.in);
-        var choice = scan.nextInt();
-        switch (choice) {
+        userChoice = scan.nextInt();
+        switch (userChoice) {
             case 1 -> Cli.greeting();
-            case 2 -> Cli.even();
+            case 2 -> FirstGame.even();
+            case 3 -> SecondGame.calc();
             case 0 -> System.out.println("Goodbye");
             default -> System.out.println("Incorrect choice");
         }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -1,42 +1,10 @@
 package hexlet.code;
 
-import java.util.Scanner;
 
 public class Cli {
-    private static String name = "";
-    public static void greeting() {
-        System.out.println("\nWelcome to the Brain Games!");
-        System.out.println("May I have your name?");
-        Scanner sc = new Scanner(System.in);
-        name = sc.nextLine();
-        System.out.println("Hello, " + name + "!");
-    }
-    public static void even() {
-        final var numberOfRounds = 3;
-        final var rangeBoundary = 101;
-        Cli.greeting();
-        System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
-        for (int i = 0; i < numberOfRounds; i++) {
-            int randomNumber = (int) (Math.random() * rangeBoundary);
-            var rightAnswer = "";
-            if (randomNumber % 2 == 0) {
-                rightAnswer = "yes";
-            } else {
-                rightAnswer = "no";
-            }
-            System.out.println("Question: " + randomNumber);
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Your answer: ");
-            var userAnswer = sc.nextLine();
-            if (userAnswer.equals(rightAnswer)) {
-                System.out.println("Correct!");
-            } else {
-                System.out.println(userAnswer + " is wrong answer ;(. Correct answer was " + rightAnswer + ".");
-                System.out.println("Let's try again, " + name + "!");
-                return;
-            }
-        }
-        System.out.println("Congratulations, " + name + "!");
 
+    public static void greeting() {
+        Engine gr = new Engine();
+        gr.greeting();
     }
 }
