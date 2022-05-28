940f18e7bd977b8201891c78ea4415347d4a16e5
Sergey Medvedev <s.medvedev86@mail.ru>
2022-05-28 11:51:15 +0300

    add util Random

---
+++ src/main/java/hexlet/code/utils/Random.java
@@ -0,0 +1,16 @@
+package hexlet.code.utils;
+
+public class Random {
+
+    public static final int DEFAULT_MAX_RANDOM_NUMBER = 30;
+
+    public static int getRandomNumber(int maxValue) {
+        return (int) ((Math.random() * maxValue) + 1);
+    }
+    public static int getRandomNumber(int minValue, int maxValue) {
+        return  (int) (Math.random() * (maxValue + 1 - minValue) + minValue);
+    }
+    public static int getRandomNumber() {
+        return (int) ((Math.random() * DEFAULT_MAX_RANDOM_NUMBER) + 1);
+    }
+}
