925a1a1328a5ce2f3093d538db484b3fb989c880
SergeiMed <s.medvedev86@mail.ru>
2022-07-10 16:04:47 +0300

    fix Cli

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -7,9 +7,9 @@ public class Cli {
     public static void greet() {
         System.out.println("\nWelcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
         System.out.println("Hello, " + name + "!");
-        scanner.close();
+        sc.close();
     }
 }
